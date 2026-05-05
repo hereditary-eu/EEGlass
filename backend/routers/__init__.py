3e9ad7f54aa32f78b49b318493edd6bfbf3aae68
MrP01 <peter@waldert.at>
2026-05-05 12:00:42 +0200

    remove shapley router accordingly

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,7 +1,6 @@
 from backend.routers.debug import debug_router as debug_router
 from backend.routers.dataset import dataset_router as dataset_router
 from backend.routers.clustering import clustering_router as clustering_router
-from backend.routers.shapley import shapley_router as shapley_router
 from backend.routers.chat import chat_router as chat_router
 from backend.routers.timeseries import timeseries_router as timeseries_router
 from backend.routers.model_router import model_router as model_router
