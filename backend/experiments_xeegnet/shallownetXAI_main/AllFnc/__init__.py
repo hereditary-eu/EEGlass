e3dce2985dee7746be764d15d8548a7527046ebb
MrP01 <peter@waldert.at>
2026-06-10 11:19:58 +0200

    cleanup experiments folder

--- backend/experiments_xeegnet/shallownetXAI_main/AllFnc/__init__.py
+++
@@ -1,8 +0,0 @@
-# from .analytics import ()
-from .split import (
-    create_nested_kfold_subject_split,
-    merge_partition_lists_noshift,
-    merge_partition_lists,
-)
-
-from .training import loadEEG
